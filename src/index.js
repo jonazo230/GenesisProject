d9e6a24cb32291f1e35882370956fb6f86e9b03b
jonazo230 <140679443+jonazo230@users.noreply.github.com>
2023-07-26 23:11:33 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://play-lh.googleusercontent.com/A26UUWOc_l_aPp2GRurp3sG0kaxjm8ArbFHtX5GQZ9x9QztmE_noNmHBE2fbTa855sZu" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
